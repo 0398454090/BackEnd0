918740e7fa5b8445238f9f28a102e3ab10091f45
Ngoc <phamphuongngoc509@gmail.com>
2024-08-10 09:42:13 +0700

    Create Users API

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -9,6 +9,25 @@ const getUsersApi = async(req, res) => {
     });
 }
 
+const postCreateUserAPI = async(req, res) => {
+
+    let email = req.body.email;
+    let name = req.body.myname;
+    let city = req.body.mycity;
+    let user = await User.create({
+        email: email,
+        name: name,
+        city: city
+    })
+
+    return res.status(200).json({
+        errorCode: 0,
+        data: user
+    });
+
+}
+
 module.exports = {
-    getUsersApi
+    getUsersApi,
+    postCreateUserAPI
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,18 +2,10 @@ const express = require('express');
 
 const routerAPI = express.Router();
 
-const { getUsersApi } = require('../controllers/apiController')
+const { getUsersApi, postCreateUserAPI } = require('../controllers/apiController')
 
-routerAPI.get('/', (req, res) => {
-    res.send(" Hello with api")
-});
-
-routerAPI.get('/abc', (req, res) => {
-    res.status(200).json({
-        data: 'hello word with apis'
-    })
-});
 
 routerAPI.get('/users', getUsersApi);
 
+routerAPI.post('/users', postCreateUserAPI);
 module.exports = routerAPI;
